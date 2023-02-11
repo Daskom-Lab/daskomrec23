da20e4c590e632d7ce0fbaa523b4c04024799ef3
Zulfikhar Akbar <108689243+Raphaeline@users.noreply.github.com>
2023-02-11 13:50:47 +0700

    Delete sidebar.js

--- html/sidebar.js
+++
@@ -1,6 +0,0 @@
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-
-closeBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-});
