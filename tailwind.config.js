d267b09e1e5a3dc84fce90857a9f8358ee537c9c
Raphaeline <zlfkhr.akbar@gmail.com>
2023-02-11 15:04:22 +0700

    add Homepage.html

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./html/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "space-milkyway": "url('/public/assets/homepage/Main_Background.png')",
+      },
+      dropShadow: {
+        "brown-shadow": "0px 0px 6px rgba(180, 113, 7, 0.8)", //use in homepage
+      },
+      fontFamily: {
+        arcade: ['"Arcade Classic"'],
+        pixel: ['"VP Pixel"'],
+      },
+    },
+  },
+  plugins: [],
+};
