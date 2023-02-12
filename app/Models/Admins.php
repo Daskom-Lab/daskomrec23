eeaf1c893c06065ec2abe77cf5c68d3ea41241ac
vincentiusantu <vincentiusantu@gmail.com>
2023-02-12 13:19:05 +0700

    modify Admins and DataCaas

--- app/Models/Admins.php
+++ app/Models/Admins.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admins extends Model
+
+class Admins extends Authenticatable
 {
     use HasFactory;
 
