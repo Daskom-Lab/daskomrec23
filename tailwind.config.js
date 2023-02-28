4951f82893f75d4bc7dce3fa38deff3d332de3a0
Raphaeline <zlfkhr.akbar@gmail.com>
2023-02-28 21:56:12 +0700

    Chore: Add Loginpage for caas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./html/*.{html,js}"],
+  content: ["./html/*.{html,js}", "./resources/**/*.blade.php", "./resources/**/*.js", "./resources/**/*.vue"],
   theme: {
     extend: {
       backgroundImage: {
         "space-milkyway": "url('/public/assets/homepage/Main_Background.png')",
-        "dark-space": "url('/public/assets/dasboardcaas/Space_Background.png')",
+        "dark-space": "url('/public/assets/Space_Background.png')",
       },
       boxShadow: {
-        "semi-sm": "0px 0px 8px 3px rgba(0, 0, 0, 0.3)", //use in homepage (login button)
+        "semi-sm": "0px 0px 8px 2px rgba(0, 0, 0, 0.3)", //use in homepage (login button)
+        "in-semi-sm": "inset 0px 0px 8px 2px rgba(0, 0, 0, 0.3)",
         "semi-lg": "0px 0px 50px 1px rgba(0, 0, 0, 0.3)",
         "semi-xl": "0px 0px 10px 1px rgba(0, 0, 0, 0.3)", //use in seidebar
       },
@@ -17,6 +18,7 @@ module.exports = {
         pixel: ['"VP Pixel"'],
       },
       width: {
+        115: "23rem",
         120: "28rem",
         130: "36rem",
         170: "50rem",
@@ -28,6 +30,12 @@ module.exports = {
       colors: {
         "dark-sky": "#00172B",
         "dream-dark": "#00111E",
+        "deep-sky": "#233B4E",
+        "dark-sun": "#f3964a",
+        "bright-sun": "#ffd0a9",
+      },
+      transitionDuration: {
+        10: "10ms",
       },
       cursor: {
         "space-ship": "url('/public/assets/cursor/space_ship.cur'), default",
